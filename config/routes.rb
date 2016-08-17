0ad0aac590490534414ae25066a6520cdd35b810
Ahmad Ghallab <ahmadghallab@outlook.com>
2016-08-17 12:32:40 +0300

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ root 'welcome#home'
 get 'about', to: 'welcome#about'
 
 resources :articles
+
+get 'signup', to: 'users#new'
+# post 'users', to: 'users#create'
+resources :users, except: [:new]
 end
